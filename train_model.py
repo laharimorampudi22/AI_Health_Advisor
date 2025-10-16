ba4475d60953439a97df94ded2ca9a1e6cf35647
laharimorampudi22 <2320030306@klh.edu.in>
2025-10-16 15:06:02 +0530

    Implement model training and saving functionality
    Added code to train a RandomForest model and save it.

--- train_model.py
+++ train_model.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+data = pd.read_csv('data/symptoms.csv')
+X = data.drop('disease', axis=1)
+y = data['disease']
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+pickle.dump(model, open('model/symptom_model.pkl', 'wb'))
+print("✅ Model trained and saved!")
 
